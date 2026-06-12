8f2b128610380ede43283ffe6d3ba74f09062f6e
rodriguesthomas <rodrigues.thomas2005@gmail.com>
2026-06-12 15:07:53 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import Photo from './components/Photo.vue';
+import Notes from './components/Notes.vue';
+import Calendrier from './components/Calendrier.vue';
+import CalendrierJour from './components/CalendrierJour.vue';
+import Message from './components/Message.vue';
+</script>
+
+<template>
+  <div class="header-cal">
+    <CalendrierJour />
+  </div>
+  <div class="center-box">
+    <div class="left-box">
+      <Notes />
+      <Calendrier />
+    </div>
+    <Photo />
+    <div class="right-box">
+      
+      <Message />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header-cal {
+  background-color: blue;
+  height: 300px;
+}
+
+.center-box {
+  background-color: blue;
+  height: 300px;
+  display : 
+}
+
+.left-box {
+  background-color: green;
+  height: 300px;
+}
+
+.left-box {
+  background-color: green;
+  height: 300px;
+}
+
+</style>
---
+++ src/components/Photo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="body">
+    <!-- Le contenu de ton app ira ici -->
+  </div>
+</template>
+
+<script setup>
+// Tes imports et ta logique ici
+</script>
+
+<style scoped>
+.body {
+  background-color: red; /* Corrigé : Syntaxe exacte CSS */
+}
+</style>
